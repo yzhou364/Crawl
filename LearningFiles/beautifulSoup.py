4c733c94d5f086bad05fad27e6bea8dd5cfb1cb4
yzhou364 <yzhou364@wisc.edu>
2022-02-26 17:11:59 -0600

    BeautifulSoup

---
+++ LearningFiles/beautifulSoup.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+domain = "https://www.dydytt.net/index2.htm"
+
+headers = {
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+}
+resp = requests.get(domain, headers = headers, verify= False)
+resp.encoding = "gb2312"
+
+page = BeautifulSoup(resp.text, "html.parser")
+
